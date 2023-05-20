cb4329126730781f81b4dddf277cf2bdd8808bed
todaykim <pztdrd@naver.com>
2023-05-20 21:48:25 +0900

    월 선택 팝업창 컴포넌트화

--- src/view/AccountBook.jsx
+++ src/view/AccountBook.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import YearPicker from "react-year-picker";
-import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import MonthModal from "../components/monthModal";
+import { Col } from "react-bootstrap";
 
 export const AccountBook = () => {
     const handleYear = (year) => {
@@ -9,75 +10,27 @@ export const AccountBook = () => {
     };
     const [isOpen, setIsOpen] = useState(false);
     const [month, setMonth] = useState("1");
-    const handleClickEvent = (month) => {
-        setMonth(month);
-        setIsOpen(false);
+    const handleButtonClick = () => {
+        setIsOpen(true);
     };
-
     return (
         <div>
-            <YearPicker onChange={handleYear} />
-            <Button onClick={() => setIsOpen(true)}>{month}월</Button>
-
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <Col xs={2}>
+                    <YearPicker onChange={handleYear} />
+                </Col>
+                <Col xs={10}>
+                    <Button
+                        variant="light"
+                        style={{ width: "100%" }}
+                        onClick={handleButtonClick}
+                    >
+                        {month}월
+                    </Button>
+                </Col>
+            </div>
             {isOpen ? (
-                <div
-                    className="modal show"
-                    style={{ display: "block", position: "initial" }}
-                >
-                    <Modal.Dialog>
-                        <Modal.Header
-                            closeButton
-                            onHide={() => {
-                                setIsOpen(false);
-                            }}
-                        >
-                            <Modal.Title>Modal title</Modal.Title>
-                        </Modal.Header>
-
-                        <Modal.Body>
-                            <p>Modal body text goes here.</p>
-                        </Modal.Body>
-
-                        <Modal.Footer>
-                            <Button onClick={() => handleClickEvent("1")}>
-                                1월
-                            </Button>
-                            <Button onClick={() => handleClickEvent("2")}>
-                                2월
-                            </Button>
-                            <Button onClick={() => handleClickEvent("3")}>
-                                3월
-                            </Button>
-                            <Button onClick={() => handleClickEvent("4")}>
-                                4월
-                            </Button>
-                            <Button onClick={() => handleClickEvent("5")}>
-                                5월
-                            </Button>
-                            <Button onClick={() => handleClickEvent("6")}>
-                                6월
-                            </Button>
-                            <Button onClick={() => handleClickEvent("7")}>
-                                7월
-                            </Button>
-                            <Button onClick={() => handleClickEvent("8")}>
-                                8월
-                            </Button>
-                            <Button onClick={() => handleClickEvent("9")}>
-                                9월
-                            </Button>
-                            <Button onClick={() => handleClickEvent("10")}>
-                                10월
-                            </Button>
-                            <Button onClick={() => handleClickEvent("11")}>
-                                11월
-                            </Button>
-                            <Button onClick={() => handleClickEvent("12")}>
-                                12월
-                            </Button>
-                        </Modal.Footer>
-                    </Modal.Dialog>
-                </div>
+                <MonthModal setIsOpen={setIsOpen} setMonth={setMonth} />
             ) : null}
         </div>
     );
