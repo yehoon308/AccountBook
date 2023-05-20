5319c753ce138c2f21d576bb42d18e832ebce5f0
todaykim <pztdrd@naver.com>
2023-05-20 19:36:55 +0900

    test push

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -3,6 +3,7 @@ import SignIn from "../components/signIn";
 import Button from "react-bootstrap/Button";
 
 function Login(props) {
+    console.log("test");
     return (
         <div style={{ display: "flex", height: "80vh" }}>
             <SignIn />
