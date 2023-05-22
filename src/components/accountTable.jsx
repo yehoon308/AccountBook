fa8dfa2aff90da4a710b89312c83b98d0efab42d
yehoon308 <127646165+yehoon308@users.noreply.github.com>
2023-05-22 12:56:39 +0900

    가계부 수정 버튼 작업중

--- src/components/accountTable.jsx
+++ src/components/accountTable.jsx
@@ -1,54 +1,66 @@
 import Table from "react-bootstrap/Table";
+import React from "react";
+import EditValue from "./editValue";
 
 function AccountTable(props) {
-    function getDaysInMonth(year, month) {
-        const date = new Date(year, month, 0);
-        return date.getDate();
+  function getDaysInMonth(year, month) {
+    const date = new Date(year, month, 0);
+    return date.getDate();
+  }
+
+  function getSelectedDays(year, month) {
+    const daysInMonth = getDaysInMonth(year, month);
+    const days = [];
+
+    for (let day = 1; day <= daysInMonth; day++) {
+      days.push(day);
     }
 
-    function getSelectedDays(year, month) {
-        const daysInMonth = getDaysInMonth(year, month);
-        const days = [];
+    return days;
+  }
 
-        for (let day = 1; day <= daysInMonth; day++) {
-            days.push(day);
-        }
+  // 예시: 2023년 5월의 일자를 가져옵니다.
+  const selectedYear = Number(props?.selectedYear);
+  const selectedMonth = Number(props?.month);
+  console.log(selectedYear, selectedMonth);
 
-        return days;
-    }
+  const selectedDays = getSelectedDays(selectedYear, selectedMonth);
+  console.log(selectedDays);
 
-    // 예시: 2023년 5월의 일자를 가져옵니다.
-    const selectedYear = Number(props?.selectedYear);
-    const selectedMonth = Number(props?.month);
-    console.log(selectedYear, selectedMonth);
-
-    const selectedDays = getSelectedDays(selectedYear, selectedMonth);
-    console.log(selectedDays);
-
-    return (
-        <div style={{ margin: "20px" }}>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>총액</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {selectedDays?.map((list, index) => (
-                        <tr>
-                            <td>{`${list}일`}</td>
-                            <td></td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-        </div>
-    );
+  return (
+    <div style={{ margin: "20px" }}>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>일자</th>
+            <th>수입</th>
+            <th>지출</th>
+            <th>합계</th>
+            <th>구분</th>
+            <th>메모</th>
+            <th>수정</th>
+            <th>전월대비</th>
+          </tr>
+        </thead>
+        <tbody>
+          {selectedDays?.map((list, index) => (
+            <tr>
+              <td>{`${list}일`}</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <button onClick={EditValue}>수정</button>
+              </td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
 export default AccountTable;
