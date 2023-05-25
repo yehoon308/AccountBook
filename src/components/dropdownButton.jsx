6777553dc2b66fc54923b588800f27a63a022e1f
yehoon308 <127646165+yehoon308@users.noreply.github.com>
2023-05-25 16:06:03 +0900

    수정버튼 카테고리 클릭 시 (카테고리)텍스트 변경 적용

--- src/components/dropdownButton.jsx
+++ src/components/dropdownButton.jsx
@@ -1,25 +1,77 @@
 import Dropdown from "react-bootstrap/Dropdown";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function DropdownButton() {
-  return (
-    <Dropdown>
-      <Dropdown.Toggle variant="Primary" id="dropdown-basic">
-        카테고리
-      </Dropdown.Toggle>
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState("카테고리");
 
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">고정비</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">식비</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">통신비</Dropdown.Item>
-        <Dropdown.Item href="#/action-4">의료비</Dropdown.Item>
-        <Dropdown.Item href="#/action-5">의류 및 악세서리</Dropdown.Item>
-        <Dropdown.Item href="#/action-6">교육비</Dropdown.Item>
-        <Dropdown.Item href="#/action-7">여가 및 문화생활</Dropdown.Item>
-        <Dropdown.Item href="#/action-8">교통비</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  );
+    const handleOptionSelect = (option) => {
+        setSelectedOption(option);
+        setIsOpen(false);
+    };
+
+    return (
+        <Dropdown>
+            <Dropdown.Toggle
+                style={{ display: "flex", justifyContent: "center" }}
+                variant="Primary"
+                id="dropdown-basic"
+            >
+                <div>{selectedOption}</div>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                <Dropdown.Item
+                    onClick={() => handleOptionSelect("고정비")}
+                    href="#/action-1"
+                >
+                    고정비
+                </Dropdown.Item>
+                <Dropdown.Item
+                    onClick={() => handleOptionSelect("식비")}
+                    href="#/action-2"
+                >
+                    식비
+                </Dropdown.Item>
+                <Dropdown.Item
+                    onClick={() => handleOptionSelect("통신비")}
+                    href="#/action-3"
+                >
+                    통신비
+                </Dropdown.Item>
+                <Dropdown.Item
+                    onClick={() => handleOptionSelect("의료비")}
+                    href="#/action-4"
+                >
+                    의료비
+                </Dropdown.Item>
+                <Dropdown.Item
+                    onClick={() => handleOptionSelect("의류 및 악세서리")}
+                    href="#/action-5"
+                >
+                    의류 및 악세서리
+                </Dropdown.Item>
+                <Dropdown.Item
+                    onClick={() => handleOptionSelect("교육비")}
+                    href="#/action-6"
+                >
+                    교육비
+                </Dropdown.Item>
+                <Dropdown.Item
+                    onClick={() => handleOptionSelect("여가 및 문화생활")}
+                    href="#/action-7"
+                >
+                    여가 및 문화생활
+                </Dropdown.Item>
+                <Dropdown.Item
+                    onClick={() => handleOptionSelect("교통비")}
+                    href="#/action-8"
+                >
+                    교통비
+                </Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
 }
 
 export default DropdownButton;
